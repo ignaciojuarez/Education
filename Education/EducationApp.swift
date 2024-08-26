81e87bfbd6fc71d4a2be529f935f7c067a61b373
Ignacio Juarez <62676603+ignaciojuarez@users.noreply.github.com>
2024-08-26 13:08:12 -0600

    Initial Commit

---
+++ Education/EducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationApp.swift
+//  Education
+//
+//  Created by Ignacio Juarez on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct EducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
